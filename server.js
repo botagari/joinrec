48f3343eb97cf89eb2af48e74d1975cb9fb2e84e
botagari <48246327+botagari@users.noreply.github.com>
2019-10-13 19:49:52 +0200

    Update server.js

--- server.js
+++ server.js
@@ -4,11 +4,11 @@ client.on('ready',()=>{
 console.log(`Logged in!`)})
 let prefix='$'
 client.on('message',message=>{
-if(message.content.startsWith(prefix+'voice1')){
+if(message.content.startsWith(prefix+'join')){
 let room = message.guild.channels.get(`${message.content.split(" ").slice(1).join(" ")}`)
 if(!room) return message.channel.send('I Can\'t find this room :x:')
 if(!room.type === 'voice') return message.channel.send('I Can\'t find this room :x:')
-room.join().catch(err=>{return message.channel.send('no premission!')})
+room.join().catch(err=>{return message.channel.send('no premissiond!')})
 }if(message.content.toLowerCase().includes("giveaway")) {
 if(message.author.id == "294882584201003009" || message.author.id == "553212598368337921") {
 message.react("🎉")} }})
